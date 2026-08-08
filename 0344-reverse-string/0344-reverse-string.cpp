3c3a6633baf2334b3d02839ab2fc45e394f31ce7
Ayontika Pal <146437784+Ayontikapal@users.noreply.github.com>
2026-08-08 11:16:14 +0530

    Time: 0 ms (100%), Space: 27.4 MB (16.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,9 +3,6 @@ public:
     void reverseString(vector<char>& s) {
         int i=0,j=s.size()-1;
         while(i<j){
-            if(s[i]==s[j]){
-                continue;
-            }
             swap(s[i],s[j]);
             i++;
             j--;
