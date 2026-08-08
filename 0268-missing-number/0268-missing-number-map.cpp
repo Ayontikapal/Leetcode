cc0c1da84f00d627ba8149458325f8efd19b1927
Ayontika Pal <146437784+Ayontikapal@users.noreply.github.com>
2026-08-08 10:24:29 +0530

    Time: 0 ms (100%), Space: 21.9 MB (12.45%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number-map.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size(),actual=0;
+        int expected=n*(n+1)/2;
+        for(int i:nums){
+            actual+=i;
+        }
+        return expected-actual;
+    }
+};
