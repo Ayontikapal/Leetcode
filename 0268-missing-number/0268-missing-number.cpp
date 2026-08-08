e7a2fb60104ff39427923b4b7022246cddb5ed30
Ayontika Pal <146437784+Ayontikapal@users.noreply.github.com>
2026-08-08 10:16:20 +0530

    Time: 15 ms (10.25%), Space: 28.1 MB (5.16%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int, int>mp;
+        int max=nums.size(), min=0;
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(int i=min;i<=max;i++){
+            if(mp[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
