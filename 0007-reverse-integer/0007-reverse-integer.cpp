8f1d368bdbc78bc8d86e7667712e86c4276a97eb
Ayontika Pal <146437784+Ayontikapal@users.noreply.github.com>
2026-08-12 19:25:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.26%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev=0;
+        while(x!=0 && x>=INT32_MIN && x<=INT32_MAX){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
+        }
+        if (rev>INT_MAX || rev<INT_MIN){
+            return 0;
+        }
+        return rev;
+    }
+};
