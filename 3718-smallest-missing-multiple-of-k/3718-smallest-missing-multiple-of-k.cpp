0cfd888fcfbad8fcf6ac1a75f6caa560370eecd6
Ayontika Pal <146437784+Ayontikapal@users.noreply.github.com>
2026-08-25 08:02:51 +0530

    Time: 3 ms (38.06%), Space: 25.3 MB (24.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -3,8 +3,8 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
         int n=nums.size();
         unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+        for(int i:nums){
+            mp[i]++;
         }
         for(int i=1;i<=n;i++){
             int item=k*i;
