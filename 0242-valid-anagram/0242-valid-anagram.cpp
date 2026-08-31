e0b6b369180387873fe0cf403714e3ce5008e344
Ayontika Pal <146437784+Ayontikapal@users.noreply.github.com>
2026-08-31 20:57:45 +0530

    Time: 0 ms (100%), Space: 10 MB (11.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t){
-            return false;
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            mp[t[i]]--;
+        }
+        for(int i=0;i<s.length();i++){
+            if(mp[s[i]]!=0){
+                return false;
+            }
         }
         return true;
     }
